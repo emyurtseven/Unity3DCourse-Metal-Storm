0f5e941524953e0b4b43f6847f0cb0f2d06b43be
emyurtseven <emyurtseven@yahoo.com>
2023-05-04 07:20:19 +0300

    Began switching movement to bezier path follow

---
+++ Assets/Scripts/Player/PathFinder.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Manages enemy movement in preset paths
+/// </summary>
+public class PathFinder : MonoBehaviour
+{
+    List<Transform> waypoints;
+
+    [SerializeField] Transform playerPath;
+
+    // List of curves to follow
+    List<CubicBezierCurve> curveList = new List<CubicBezierCurve>();
+
+    [SerializeField] float moveSpeedMultiplier;
+
+    CubicBezierCurve currentCurve;
+
+    float moveSpeed;
+    float speedFactor;
+
+    float t = 0;
+
+    // These are for movement smoothing along the bezier curve
+    // float targetStepSize;
+    // float lastStepSize;
+
+    Vector3 previousPos;
+
+
+    void Start()
+    {
+        foreach (Transform curveTransform in playerPath)
+        {
+            CubicBezierCurve curve = curveTransform.GetComponent<CubicBezierCurve>();
+            curve.InitializeControlPoints();
+            curveList.Add(curve);
+        }
+
+        StartCoroutine(FollowPath());
+    }
+
+    private void FixedUpdate() 
+    {
+        FollowCurrentCurve();
+    }
+
+    private IEnumerator FollowPath()
+    {
+        int index = 0;
+        while(true)
+        {
+            t = 0;
+            currentCurve = curveList[index];
+            float curveLength = currentCurve.BezierSingleLength(currentCurve.ControlPointPositions);
+            moveSpeed = (Time.fixedDeltaTime / curveLength) * moveSpeedMultiplier;
+
+            yield return new WaitUntil(() => CheckCurrentCurveCompleted(t));
+
+            index++;
+
+            if (index == curveList.Count)
+            {
+                moveSpeed = 0;
+                yield break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Coroutine for following the curves
+    /// </summary>
+    void FollowCurrentCurve()
+    {
+        // Take steps every frame along the bezier curve in relation to the parameter t, 
+        // while t is smaller than the boundary condition.
+        // t starts as 0 and goes to 1, or vice versa if reversed
+
+        previousPos = transform.position;
+        transform.position = currentCurve.BezierCubic(t);
+        // ModulateSpeed();
+
+        t += moveSpeed;
+    }
+
+    /// <summary>
+    /// Check if current curve is completed, ie t = boundary
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns>Returns a bool indicating if completed</returns>
+    private bool CheckCurrentCurveCompleted(float t)
+    {
+        return (t >= 1);
+    }
+
+    /// <summary>
+    /// Smmothe the movement speed along the bezier curve
+    /// </summary>
+    // void ModulateSpeed()
+    // {
+    //     lastStepSize = Vector3.Magnitude(transform.position - previousPos);
+    //     if (lastStepSize < targetStepSize)
+    //     {
+    //         speedFactor *= 1.1f;
+    //     }
+    //     else
+    //     {
+    //         speedFactor *= 0.9f;
+    //     }
+    // }
+}
---
+++ Assets/Scripts/Utilities/CubicBezierCurve.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+/// <summary>
+/// Attached to a curve object, has 4 coordinates for calculating a cubic bezier curve.
+/// Draw gizmos in scene view to visualize the curve. 
+/// </summary>
+public class CubicBezierCurve : MonoBehaviour
+{
+    Transform[] controlPoints = new Transform[4];
+    Vector3[] controlPointPositions = new Vector3[4];
+    Vector3 gizmosPosition;
+
+    public Transform[] Waypoints
+    {
+        get { return controlPoints; }
+    }
+
+    public Vector3[] ControlPointPositions 
+    { 
+        get 
+        {
+            int i = 0;
+            foreach (Transform controlPoint in controlPoints)
+            {
+                controlPointPositions[i] = controlPoint.position;
+                i++;
+            }
+            return controlPointPositions;
+        }
+    }
+
+    /// <summary>
+    /// Find the child objects which are points in scene and store their coordinates
+    /// </summary>
+    public void InitializeControlPoints()
+    {
+        int index = 0;
+        foreach (Transform child in transform)
+        {
+            controlPoints[index] = child;
+            index++;
+        }
+    }
+
+    /// <summary>
+    /// Apply bezier transform to parameter t
+    /// Formula: https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Cubic_B%C3%A9zier_curves
+    /// </summary>
+    /// <param name="t"> Parameter t, 0->1 or vice versa </param>
+    /// <returns> Returns 2D coordinates of resultant transformation </returns>
+    public Vector3 BezierCubic(float t)
+    {
+        Vector3 component1 = Mathf.Pow((1 - t), 3) * controlPoints[0].position;
+        Vector3 component2 = 3 * Mathf.Pow((1 - t), 2) * t * controlPoints[1].position;
+        Vector3 component3 = 3 * (1 - t) * Mathf.Pow(t, 2) * controlPoints[2].position;
+        Vector3 component4 = Mathf.Pow(t, 3) * controlPoints[3].position;
+
+        Vector3 currentPos = component1 + component2 + component3 + component4;
+
+        return currentPos;
+    }
+
+    public float BezierSingleLength(Vector3[] points)
+    {
+        var p0 = points[0] - points[1];
+        var p1 = points[2] - points[1];
+        var p2 = new Vector3();
+        var p3 = points[3]-points[2];
+
+        var l0 = p0.magnitude;
+        var l1 = p1.magnitude;
+        var l3 = p3.magnitude;
+        if(l0 > 0) p0 /= l0;
+        if(l1 > 0) p1 /= l1;
+        if(l3 > 0) p3 /= l3;
+
+        p2 = -p1;
+        var a = Mathf.Abs(Vector3.Dot(p0,p1)) + Mathf.Abs(Vector3.Dot(p2,p3));
+        if(a > 1.98f || l0 + l1 + l3 < (4 - a)*8) return l0+l1+l3;
+
+        var bl = new Vector3[4];
+        var br = new Vector3[4];
+
+        bl[0] = points[0];
+        bl[1] = (points[0]+points[1]) * 0.5f;
+
+        var mid = (points[1]+points[2]) * 0.5f;
+
+        bl[2] = (bl[1]+mid) * 0.5f;
+        br[3] = points[3];
+        br[2] = (points[2]+points[3]) * 0.5f;
+        br[1] = (br[2]+mid) * 0.5f;
+        br[0] = (br[1]+bl[2]) * 0.5f;
+        bl[3] = br[0];
+
+        return BezierSingleLength(bl) + BezierSingleLength(br);
+    }
+
+    /// <summary>
+    /// Update drawn gizmos in scene view for visual help
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        InitializeControlPoints();
+
+        Gizmos.color = Color.yellow;
+        for (float t = 0; t <= 1; t += 0.05f)
+        {
+            Gizmos.DrawSphere(BezierCubic(t), 1f);
+        }
+
+        Gizmos.DrawLine(new Vector3(controlPoints[0].position.x, controlPoints[0].position.y, controlPoints[0].position.z), new Vector3(controlPoints[1].position.x, controlPoints[1].position.y, controlPoints[1].position.z));
+
+        Gizmos.DrawLine(new Vector3(controlPoints[2].position.x, controlPoints[2].position.y, controlPoints[2].position.z), new Vector3(controlPoints[3].position.x, controlPoints[3].position.y, controlPoints[3].position.z));
+
+    }
+}
